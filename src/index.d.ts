5be6055d36efdb2e779d160a348fdb1fa88268fe
mhazaa <magdihazaa@gmail.com>
2021-12-19 01:05:03 -0600

    adding declaration files

---
+++ src/index.d.ts
@@ -0,0 +1,28 @@
+import * as MongoDB from 'mongodb';
+interface Config {
+    DBUSERNAME: string;
+    DBPASSWORD: string;
+    DBCLUSTERNAME: string;
+    DBNAME: string;
+}
+declare class Collection {
+    mongoCollection: MongoDB.Collection;
+    constructor(mongoCollection: MongoDB.Collection);
+    getAllDocuments(): Promise<MongoDB.Document[]>;
+    getDocumentById(id: string): Promise<MongoDB.Document>;
+    getDocumentByField(field: string): Promise<MongoDB.Document | null>;
+    insertOne(data: object): Promise<MongoDB.Document>;
+    createOrUpdateDocument(query: object, update: object): Promise<MongoDB.Document>;
+}
+declare class MagsMongoDB {
+    private config;
+    private url;
+    private client;
+    private db;
+    constructor(config: Config);
+    private getUrl;
+    connect(): Promise<MongoDB.Db>;
+    collection(collectionName: string): Collection;
+}
+export { Config, Collection };
+export default MagsMongoDB;
